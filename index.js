72006502c182b0bd9d9be1d6137a88c470173689
Skylar <skylarblessings5@gmail.com>
2023-03-23 02:21:55 +0300

    assignment

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const batteryBatches = [15, 10, 5];
+
+const totalBatteries = batteryBatches.reduce((acc, curr) => acc + curr, 0);
+
+console.log(totalBatteries); // Output: 30
